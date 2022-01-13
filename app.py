fc530dd7a4ca2f11956e711e5f90c5bac7a55e7c
Jahidul Alam <mjahidulalam@gmail.com>
2022-01-13 13:22:12 -0700

    uodated requirements

--- app.py
+++ app.py
@@ -15,10 +15,9 @@ page_style = """
             padding-bottom: 10px;
             width: 80vw !important;
         }
-
     </style>
-
 """
+
 st.markdown(page_style, unsafe_allow_html=True)
 
 labels_dict = {0:"A", 1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J",10:"K",
